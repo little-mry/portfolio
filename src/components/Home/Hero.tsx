8d8a91305da287f874ed77ecb1730f32f1a40236
Lilla Mry <maria.kjellholm@gmail.com>
2025-09-04 11:45:51 +0200

    changed heroimg

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
       <img
         src={HeroImg}
         alt="Image of Maria, a person with long blond hair and glasses, smiling"
-        className="block mx-auto w-40 sm:w-56 md:w-72 lg:w-80 h-auto"
+        className="block mx-auto w-40 sm:w-72 md:w-80 lg:w-90 h-auto"
       />
     </section>
   );
