f3d0051aec0b27fb78f5bae716c0223b4198cf5b
Lilla Mry <maria.kjellholm@gmail.com>
2025-09-04 11:31:07 +0200

    changed heroimg

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -1,11 +1,11 @@
-import HeroImg from "../../assets/mariakj-img.png";
+import HeroImg from "../../assets/mariakj-sitting.png";
 
 const Hero = () => {
   return (
     <section className="w-full">
       <img
         src={HeroImg}
-        alt="Image of Maria- a person with long blond hair in a braid, glasses and a yellow tshirt"
+        alt="Image of Maria, a person with long blond hair and glasses, smiling"
         className="block mx-auto w-40 sm:w-56 md:w-72 lg:w-80 h-auto"
       />
     </section>
